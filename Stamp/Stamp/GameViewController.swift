6d2ceb292884374d8f21646ddcf8946e2392a37a
246_MBP <nishimu@246-MBP.local>
2021-11-30 16:21:21 +0900

    とりあえずNFCID取れた

--- Stamp/Stamp/GameViewController.swift
+++ Stamp/Stamp/GameViewController.swift
@@ -10,11 +10,18 @@ import NCMB
 import CoreNFC
 
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController, NFCTagReaderSessionDelegate {
+    
+    var session: NFCReaderSession?
+    
+    
+    @IBOutlet weak var UIDLabel1: UILabel!
     
     var game_data:[NCMBObject] = [] //スタンプ情報
     var sNum:Int = 0    //選択したマップの番号
-    var className:String? = ""
+    var className:String? = "" //検索するクラス名
+    var tagID:[String] = [] //tagID保存
+    var NFCNum:Int = 0 //NFCタグの個数
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +37,14 @@ class GameViewController: UIViewController {
             switch result {
                 case let .success(array):
                     print("取得に成功しました")
-                                        
+                    self.NFCNum = array.count
+                    self.game_data = array
+                    
+                    for i in 0..<self.NFCNum{
+                        let tID:String? = array[i]["tagID"]
+                        self.tagID.append(tID!)
+                    }
+                    
                 case let .failure(error):
                     print("取得に失敗しました: \(error)")
             }
@@ -39,6 +53,44 @@ class GameViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    
+    @IBAction func CaptureBtn(_ sender: Any) {
+        self.session = NFCTagReaderSession(pollingOption: .iso14443, delegate: self)
+        self.session?.alertMessage = "Hold Your Phone Near The NFC Tag"
+        self.session?.begin()
+    }
+    
+    func tagReaderSessionDidBecomeActive(_ session: NFCTagReaderSession) {
+        print("session begun!")
+    }
+    
+    func tagReaderSession(_ session: NFCTagReaderSession, didInvalidateWithError error: Error) {
+        print("error with launching session")
+    }
+    
+    func tagReaderSession(_ session: NFCTagReaderSession, didDetect tags: [NFCTag]) {
+        print("connecting to Tag")
+        if tags.count > 1 {
+            session.alertMessage = "More Than One Tag "
+            session.invalidate()
+        }
+        let tag = tags.first!
+        session.connect(to: tag){ (error) in
+            if nil != error{
+                session.invalidate(errorMessage: "Connection Failed")
+            }
+            if case let .miFare(sTag) = tag{
+                let UID = sTag.identifier.map{ String(format: "%.2hhx", $0)}.joined()
+                print("UID:",UID)
+                print(sTag.identifier)
+                session.alertMessage = "UID Captured"
+                session.invalidate()
+                DispatchQueue.main.async {
+                    self.UIDLabel1.text = "\(UID)"
+                }
+            }
+        }
+    }
 
     /*
     // MARK: - Navigation
